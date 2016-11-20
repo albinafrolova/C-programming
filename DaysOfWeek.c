36959db39e026f9b274ce776ccbabdf51fb42af3
Альбина Фролова <afrolova@na.edu>
2016-11-19 23:38:54 -0600

    Added program that prints days of the week

---
+++ DaysOfWeek.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	//Local Declarations
+	char * pDays[7];
+	char** pLast;
+
+	//Statements
+	pDays[0] = "Sunday";
+	pDays[1] = "Monday";
+	pDays[2] = "Tuesday";
+	pDays[3] = "Wednesday";
+	pDays[4] = "Thursday";
+	pDays[5] = "Friday";
+	pDays[6] = "Saturday";
+
+	printf("The days of the week: \n");
+	pLast = pDays + 6;
+	for (char** pWalker = pDays; pWalker <= pLast; pWalker++)
+		printf("%s\n", *pWalker);
+	return 0;
+} //main
