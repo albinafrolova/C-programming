200b92f98a9d229f4cb312c8ced77056e3710381
Albina Frolova <afrolova@na.edu>
2017-03-29 22:14:21 -0500

    Update Qr.c

--- Qr.c
+++ Qr.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-// Function Declaration 
 void getData (int* pa, int* pb, int* pc);
 int quadratic (int a, int b , int c, double* pRoot1, double* pRoot2);
 
@@ -9,7 +8,6 @@ void printResults (int numRoots, int a, int b, int c, double root1, double root2
 
 int main (void)
 {
-// Local Declaration
 	int a;
 	int b; 
 	int c;
@@ -18,7 +16,6 @@ int main (void)
 	double root2;
 	char again = 'Y';
 
-// Statements
 	printf("Solve quadratic equations.\n\n");
 	while (again == 'Y' || again == 'y')
 	{
@@ -28,27 +25,24 @@ int main (void)
 
 		printf("\nDo you have another equation (Y/N): ");
 		scanf(" %c ", &again);
-	} // while
+	} 
 	printf("\nThank you.\n");
 	return 0;
-} // main 
+}
 
 void getData (int* pa, int* pb, int* pc)
 {
-// Statements 
 	printf("Please enter coefficients a, b, & c: ");
 	scanf("%d%d%d", pa, pb, pc);
-
 	return; 
-} // getData
+} 
 
 int quadratic (int a, int b, int c, double* pRoot1, double* pRoot2)
 {
 	int result;
 	double discriminant;
 	double root;
-
-// Statements 
+	
 	if (a == 0 && b == 0)
 		result = -1;
 	else 
@@ -56,7 +50,7 @@ int quadratic (int a, int b, int c, double* pRoot1, double* pRoot2)
 		{
 			*pRoot1 = -c / (double) b;
 			result  = 1;
-		} // a == 0
+		} 
 	else 
 	{
 		discriminant = b * b - (4 * a * c);
@@ -66,16 +60,15 @@ int quadratic (int a, int b, int c, double* pRoot1, double* pRoot2)
 			*pRoot1 = (-b + root) / (2 * a);
 			*pRoot2 = (-b - root) / (2 * a);
 			result = 2;
-		} // if >= 0
+		} 
 	else 
 		result = 0;
-	} // else 
+	}  
 return result; 
-} // quadratic 
+} 
 
 void printResults (int numRoots, int a, int b, int c, double root1, double root2)
 {
- // Statements 
 	printf("Your equation: %dx**2 + %dx + %d\n", a,b,c);
 	switch (numRoots)
 	{
@@ -87,6 +80,6 @@ void printResults (int numRoots, int a, int b, int c, double root1, double root2
 				break;
 		default: printf("Invalid coefficients\n");
 				break;
-	} // switch 
+	} 
 return;
-} // printResults 
+} 
